48986f55b4736fad2d43e797fac6cfcfd333a226
Camelia Ciuca <camelia1414@gmail.com>
2025-11-19 11:17:31 +0100

    added test_users

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -7,10 +7,10 @@ from backend.app.main import app
 
 client = TestClient(app)
 
-def test_health ():
-    response = client.get("/api/health")
+def test_get_users():
+    response = client.get("/api/users")
     assert response.status_code == 200
-    data = response.json()
-    assert data["status"] == "ok"
-    assert data["service"] == "FastAPI backend"
-
+    users = response.json()
+    assert isinstance(users,list)
+    assert len(users) > 0
+    assert "name" in users[0]
