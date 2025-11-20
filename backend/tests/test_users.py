04cc86f9e0c1195c640dfdd43d39d167022f1bc5
Camelia Ciuca <camelia1414@gmail.com>
2025-11-20 13:04:48 +0100

    test get_user_by_id

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -7,6 +7,9 @@ from backend.app.main import app
 
 client = TestClient(app)
 
+# ----------------------
+# POZITIVE TESTS
+# ----------------------
 def test_get_all_users():
     response = client.get("/api/users")
     assert response.status_code == 200
@@ -14,5 +17,13 @@ def test_get_all_users():
     users = response.json()
     assert isinstance(users,list)
     assert len(users) == 4
-    
+
+def test_get_user_by_id():
+    response = client.get("/api/users", params={"id": 1})
+    assert response.status_code == 200
+
+    users = response.json()
+    assert len(users) == 1
+    assert users[0]["name"] == "John Doe"
+
     
